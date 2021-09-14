5ef90c31f92b3d0f2d5711f6ac1f30218fb53f62
bmastahac <bogdan.mastahac@gmail.com>
2021-09-15 01:50:44 +0300

    Added Nonce management

--- EthAdapter/anchoring/addAnchorSmartContract.js
+++ EthAdapter/anchoring/addAnchorSmartContract.js
@@ -6,39 +6,54 @@ function addAnchor(anchorFactory, account,
     //Concurrent transactions
     //https://github.com/ChainSafe/web3.js/issues/1301
     //fixed in 2.0, but is alpha
-    anchorFactory.getTransactionCount(account,"pending", (err, count) =>{
-        if (err){
-            console.log('Failed to get the next nonce', err);
-            return callback(err);
+
+    anchorFactory.eth.getTransactionCount(account,"pending").then(
+        (nonce) => {
+            const nextNonce = nonce;
+            console.log('Nonce obtained :', nextNonce);
+
+            addAnchorInternal(anchorFactory, account,
+                anchorID, controlString,
+                newHashLinkSSI, ZKPValue, lastHashLinkSSI,
+                signature, publicKey,nextNonce, callback);
         }
-        const nextNonce = count;
-        console.log('Nonce obtained :', nextNonce);
-        console.log('Input for addAnchor smart contract : ',anchorID,controlString,
-            newHashLinkSSI, ZKPValue, lastHashLinkSSI,
-            signature, publicKey);
-        anchorFactory.methods.addAnchor(anchorID, controlString,
-            newHashLinkSSI, ZKPValue, lastHashLinkSSI,
-            signature, publicKey).send({from: account, gas: 1500000, nonce: nextNonce}).then((f) => {
-            const statusCode = f.events.InvokeStatus.returnValues.statusCode.toString().trim();
-            console.log("Smart contract status code : ", statusCode);
-            if (statusCode === "200" || statusCode === "201") {
-                callback(null, "Success");
-            } else {
-                console.log("execute callback error : status code : <", statusCode, ">, EVAL :", statusCode === "200");
-                callback(new Error("Status Code " + statusCode), null);
-            }
-        })
-            .catch(err => {
-                console.log({
-                    anchorID,
-                    controlString, newHashLinkSSI, ZKPValue, lastHashLinkSSI, signature, publicKey, account
-                });
-                console.log(err);
-                callback(err, null);
-            });
-    })
+    ).catch((err) => {
+        console.log (err);
+        callback(err, null);
+    });
 
 }
+
+function addAnchorInternal(anchorFactory, account,
+                           anchorID, controlString,
+                           newHashLinkSSI, ZKPValue, lastHashLinkSSI,
+                           signature, publicKey,nextNonce,
+                           callback){
+    console.log('Input for addAnchor smart contract : ',anchorID,controlString,
+        newHashLinkSSI, ZKPValue, lastHashLinkSSI,
+        signature, publicKey);
+    anchorFactory.contract.methods.addAnchor(anchorID, controlString,
+        newHashLinkSSI, ZKPValue, lastHashLinkSSI,
+        signature, publicKey).send({from: account, gas: 1500000, nonce: nextNonce}).then((f) => {
+        const statusCode = f.events.InvokeStatus.returnValues.statusCode.toString().trim();
+        console.log("Smart contract status code : ", statusCode);
+        if (statusCode === "200" || statusCode === "201") {
+            callback(null, "Success");
+        } else {
+            console.log("execute callback error : status code : <", statusCode, ">, EVAL :", statusCode === "200");
+            callback(new Error("Status Code " + statusCode), null);
+        }
+    })
+        .catch(err => {
+            console.log({
+                anchorID,
+                controlString, newHashLinkSSI, ZKPValue, lastHashLinkSSI, signature, publicKey, account
+            });
+            console.log(err);
+            callback(err, null);
+        });
+}
+
 
 
 module.exports = addAnchor;
