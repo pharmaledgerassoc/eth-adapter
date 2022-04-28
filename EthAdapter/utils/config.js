b49975531012a989c2c10d9ac56a89ad0b11e06f
skutner <sava.dumitru.daniel@gmail.com>
2022-04-28 18:38:06 +0300

    added log for debugging

--- EthAdapter/utils/config.js
+++ EthAdapter/utils/config.js
@@ -1,49 +1,47 @@
-
-
-
-
 module.exports = function Config(callback) {
 
     //config map
-    if (typeof process.env.SMARTCONTRACTADDRESS !== "undefined")
-    {
+    if (typeof process.env.SMARTCONTRACTADDRESS !== "undefined") {
         console.log('Using env SMARTCONTRACTADDRESS : ', process.env.SMARTCONTRACTADDRESS);
-        this.contractAddress =  process.env.SMARTCONTRACTADDRESS;
-    }else {
+        this.contractAddress = process.env.SMARTCONTRACTADDRESS;
+    } else {
         return callback(
             new Error("SMARTCONTRACTADDRESS not found."))
     }
 
     //config map
-    if (typeof process.env.SMARTCONTRACTABI !== "undefined")
-    {
+    if (typeof process.env.SMARTCONTRACTABI !== "undefined") {
         console.log('Using env SMARTCONTRACTABI : ', process.env.SMARTCONTRACTABI);
-        this.abi =  JSON.parse(process.env.SMARTCONTRACTABI);
-    }else {
+        this.abi = JSON.parse(process.env.SMARTCONTRACTABI);
+    } else {
         return callback(new Error("SMARTCONTRACTABI not found."))
     }
 
     //config map
-    if (typeof process.env.RPC_ADDRESS !== "undefined")
-    {
+    if (typeof process.env.RPC_ADDRESS !== "undefined") {
         console.log('Using env RPC_ADDRESS : ', process.env.RPC_ADDRESS);
-        this.rpcAddress =  process.env.RPC_ADDRESS;
-    }else {
+        this.rpcAddress = process.env.RPC_ADDRESS;
+    } else {
         return callback(new Error("RPC_ADDRESS not found."))
     }
 
     //secrets - ORGACCOUNT
-    if (typeof process.env.ORGACCOUNT !== "undefined")
-    {
+    if (typeof process.env.ORGACCOUNT !== "undefined") {
         console.log('Using env ORGACCOUNT : ', process.env.ORGACCOUNT)
-        const orgacc = JSON.parse(process.env.ORGACCOUNT);
+        let orgacc;
+        try {
+            orgacc = JSON.parse(process.env.ORGACCOUNT);
+        } catch (e) {
+            return callback(e);
+        }
+        console.log("orgacc", orgacc, typeof orgacc, Object.keys(orgacc));
         console.log("Address", orgacc.address, orgacc["address"]);
         console.log("PrivateKey", orgacc.privateKey, orgacc["privateKey"]);
-        this.account =  orgacc.address;
+        this.account = orgacc.address;
         this.accountPrivateKey = orgacc.privateKey;
         console.log("config state after setting account credentials", this.account, this.accountPrivateKey);
         console.log(this);
-    }else {
+    } else {
         return callback(new Error("ORGACCOUNT not found."))
     }
 
